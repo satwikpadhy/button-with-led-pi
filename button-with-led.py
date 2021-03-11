c3cbe3108208c10d0acd6a9f850b8f251743cf24
satwikpadhy <33980070+satwikpadhy@users.noreply.github.com>
2021-03-11 14:36:13 +0530

    Update button-with-led.py

--- button-with-led.py
+++ button-with-led.py
@@ -1,12 +1,17 @@
+import os
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-GPIO.setup(7, GPIO.IN) # Set pin 7 to be an input pin
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Pin 8 for LED
+GPIO.setup(7, GPIO.IN) # Pin 7 for the button
 
 while True:
     if GPIO.input(7):
+        print("Button Pressed!")
         GPIO.output(8, GPIO.HIGH) # If pin 7 is high, turn on pin 8
-    else
-        GPIO.output(8, GPIO>LOW) # If pin 7 is low, turn off pin 8
+        sleep(0.2)
+        os.system('clear')
+    else:
+        GPIO.output(8, GPIO.LOW) # If pin 7 is low, turn off pin 8
+        sleep(0.2)
